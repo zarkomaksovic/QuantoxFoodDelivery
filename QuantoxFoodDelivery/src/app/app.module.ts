afa92ad35f4cc68dae90e41896e2be1c77dd2a6b
Lazarlb <lazars310@gmail.com>
2019-04-17 12:43:26 +0200

    header,bootstrap and material

--- QuantoxFoodDelivery/src/app/app.module.ts
+++ QuantoxFoodDelivery/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
