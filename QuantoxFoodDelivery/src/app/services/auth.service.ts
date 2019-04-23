56bca281d19686ef29652d0b9cfe2b24bcb226cc
Zarko Maksovic <maksokv@gmail.com>
2019-04-23 16:33:01 +0200

    authentication implemented in appcomponent

--- QuantoxFoodDelivery/src/app/services/auth.service.ts
+++ QuantoxFoodDelivery/src/app/services/auth.service.ts
@@ -3,9 +3,7 @@ import { Subject } from 'rxjs';
 import { AngularFireAuth } from 'angularfire2/auth'
 
 import { AuthData } from '../models/auth-data.model';
-import { User } from '../models/user.model';
 import { Injectable } from '@angular/core';
-import { error } from '@angular/compiler/src/util';
 
 @Injectable()
 export class AuthService {
@@ -14,13 +12,26 @@ export class AuthService {
 
   constructor(private router: Router, private afAuth: AngularFireAuth) {}
 
+  initAuthListener() {
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.isAuthenticated = true;
+        this.authChange.next(true);
+        this.router.navigate(['/restaurants']);
+      } else {
+        this.authChange.next(false);
+        this.router.navigate(['/login']);
+        this.isAuthenticated = false;
+      }
+    });
+  }
   registerUser(authData: AuthData) {
     this.afAuth.auth.createUserWithEmailAndPassword(
       authData.email,
       authData.password
       ).then(result => {
         console.log(result);
-        this.authSuccessfully();
+
       })
       .catch( error => {
         console.log(error);
@@ -31,28 +42,19 @@ export class AuthService {
     this.afAuth.auth.signInWithEmailAndPassword(
       authData.email,
       authData.password
-      ).then(result => {
-        this.authSuccessfully();
-      })
+      )
       .catch( error => {
         console.log(error);
       });
   }
 
   logout() {
-    this.authChange.next(false);
-    this.router.navigate(['/login']);
-    this.isAuthenticated = false;
+    this.afAuth.auth.signOut();
   }
-
 
   isAuth() {
     return this.isAuthenticated;
   }
-  authSuccessfully() {
-    this.isAuthenticated = true;
-    this.authChange.next(true);
-    this.router.navigate(['/restaurants']);
-  }
+
 }
 
